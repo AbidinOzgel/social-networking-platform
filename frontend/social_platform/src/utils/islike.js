0883fc431aff7508f33eb80792c28cba7ab84f66
Abidin özgel <abidinozgel@gmail.com>
2024-12-07 16:33:12 +0300

    Update islike.js

--- frontend/social_platform/src/utils/islike.js
+++ frontend/social_platform/src/utils/islike.js
@@ -1,8 +1,8 @@
 export const islike = (requestUserId, post) => {
-    // liked'in array olup olmadığını kontrol et
+
     const liked = Array.isArray(post.liked) ? post.liked : [];
     
-    // array üzerinde işlem yap
+
     for (let user of liked) {
         if (requestUserId === user.id) {
             return true;
